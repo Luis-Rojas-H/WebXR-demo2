3ba6d77029265f79fe362eaf3b0a2deb2a55155c
Luis-Rojas-H <73759009+Luis-Rojas-H@users.noreply.github.com>
2024-11-29 13:17:42 -0500

    Update main.js

--- static/main.js
+++ static/main.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const mindarThree = new window.MINDAR.IMAGE.MindARThree({
       container: document.body,
-      imageTargetSrc: '/static/assets/targets/targets (6).mind',
+      imageTargetSrc: '/static/assets/targets/targets_1.mind',
     });
     const {renderer, scene, camera} = mindarThree;
 
